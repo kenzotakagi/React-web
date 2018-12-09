719d1695e887976f1816900debcf245683970262
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-12-09 15:03:37 +0900

    Fragment Component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -5,7 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const Hello = () => {
-  return <div>こんにちは、坂本龍馬さん</div>;
+  return (
+    <React.Fragment>
+      <div>こんにちは、坂本龍馬さん</div>
+      <div>こんにちは、坂本龍馬さん</div>
+    </React.Fragment>
+  );
 };
 
 const helloElement = <Hello />;
