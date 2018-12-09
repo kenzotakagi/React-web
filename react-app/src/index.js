09f66a1b47bd1e58a0fb799c19403c5d774fc991
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-12-09 15:26:48 +0900

    multiple values

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,28 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const Hello = (props) => {
+const Profile = (props) => {
   return (
-    <React.Fragment>
-      <div>こんにちは、{props.children}さん</div>
-    </React.Fragment>
-  );
-};
-
-const Greeting = (props) => {
-  return (
-    <div>
-      <div>ご挨拶</div>
-      { props.children }
-    </div>
+    <ul>
+      <li>名前: {props.name}</li>
+      <li>誕生日: {props.birthDay}</li>
+    </ul>
   )
 }
-
+  
 ReactDOM.render(
-  <Greeting>
-    <Hello>坂本龍馬</Hello>
-    <Hello>西郷隆盛</Hello>
-  </Greeting>,
+  <Profile name='坂本龍馬' birthDay='1836/01/03' />,
   document.getElementById("root")
 );
 
