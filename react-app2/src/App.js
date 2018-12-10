7aeba2a2553b0af209c7fbd0b6206caf9a436147
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-12-10 14:54:48 +0900

    set state

--- react-app2/src/App.js
+++ react-app2/src/App.js
@@ -3,7 +3,6 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -15,17 +14,29 @@ class App extends Component {
     };
   }
 
-  render() {
-    const tasks = [
-      { title: 'Todo1', id: 0 },
-      { title: 'Todo2', id: 1 },
-    ];
+  addTodo(title) {
+    const {
+      tasks,
+      uniqueID
+    } = this.state;
 
+    tasks.push({
+      title,
+      id: uniqueID,
+    });
+
+    this.setState({
+      tasks,
+      uniqueID: uniqueID + 1,
+    });
+  }
+
+  render() {
     return (
       <div>
         <h1>TODO App</h1>
         <TodoInput />
-        <TodoList tasks={tasks} />
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
