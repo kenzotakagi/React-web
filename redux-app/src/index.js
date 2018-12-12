0b531833f25961a13cd60926aaa7c8719c3e2678
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-12-12 23:51:03 +0900

    redux

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -3,10 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+const initialState = {
+  tasks: []
+};
+
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.task])
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(tasksReducer);
+
+function handleChange() {
+  console.log(store.getState());
+}
+
+const unsubscribe = store.subscribe(handleChange)
+unsubscribe()
+
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+console.log(store.getState());
+
+store.dispatch(addTask('Storeを学ぶ'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
