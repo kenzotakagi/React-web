3a6e62cf6a12490de8712e66468ab4e43b0a366b
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-01-12 15:55:30 +0900

    5.1 5.2

--- redux-app2/src/index.js
+++ redux-app2/src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const initialState = {
+  tasks: []
+};
+
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+    return {
+      ...state,
+      tasks: state.tasks.concat([action.task])
+    };
+    default:
+    return state;
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
