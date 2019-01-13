14e5533694ac9bb448e2d130a91d8165bb8b6e89
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-01-13 22:15:13 +0900

    delete reacts code

--- redux-app2/src/index.js
+++ redux-app2/src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
 const initialState = {
   tasks: []
 };
+
+const store = createStore(tasksReducer);
 
 function tasksReducer(state = initialState, action) {
   switch (action.type) {
@@ -26,10 +24,3 @@ const addTask = (task) => ({
     task
   }
 })
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
