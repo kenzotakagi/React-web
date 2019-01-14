bf3708bb5b2946deaff5dd5f5dfc5b2bc305227d
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-01-14 22:35:01 +0900

    5.18

--- redux-app2/src/index.js
+++ redux-app2/src/index.js
@@ -1,29 +1,70 @@
+import React from 'react';
+import { render } from 'react-dom';
 import { createStore } from 'redux';
 
 const initialState = {
+  task: '',
   tasks: []
 };
 
-const store = createStore(tasksReducer);
-
 function tasksReducer(state = initialState, action) {
   switch (action.type) {
+    case 'INPUT_TASK':
+      return {
+        ...state,
+        task: action.payload.task
+      };
     case 'ADD_TASK':
-    return {
-      ...state,
-      tasks: state.tasks.concat([action.task])
-    };
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task])
+      };
     default:
-    return state;
+      return state;
   }
 }
 
-const addTask = (task) => ({
-  type: 'ADD_TASK',
+const store = createStore(tasksReducer);
+
+const inputTask = task => ({
+  type: "INPUT_TASK",
   payload: {
     task
   }
-})
+});
 
-store.dispatch(addTask("storeを学ぶ"))
-console.log(store.getState())
+const addTask = task => ({
+  type: "ADD_TASK",
+  payload: {
+    task
+  }
+});
+
+function TodoApp({ store }) {
+  const { task, tasks } = store.getState();
+  return (
+    <div>
+      <input type="text" onChange={ (e) => store.dispatch(inputTask(e.target.value)) } />
+      <input type="button" value="add" onClick={ () => store.dispatch(addTask(task)) } />
+      <ul>
+        {
+          tasks.map(function (item, i) {
+            return (
+              <li key={ i }>{ item }</li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+function renderApp(store) {
+  render(
+    <TodoApp store={ store } />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(() => renderApp(store));
+renderApp(store);
